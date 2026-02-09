c0d6e9da6bbacc672149178635dfb217c963f167
TheZuTi <Daniil9426@gmail.com>
2026-02-09 15:09:16 +0200

    s

--- Content.Shared/DeadSpace/Necromorphs/CorpseCollector/SharedCorpseCollectorSystem.cs
+++ Content.Shared/DeadSpace/Necromorphs/CorpseCollector/SharedCorpseCollectorSystem.cs
@@ -1,7 +1,7 @@
 // Мёртвый Космос, Licensed under custom terms with restrictions on public hosting and commercial use, full text: https://raw.githubusercontent.com/dead-space-server/space-station-14-fobos/master/LICENSE.TXT
 
 using Content.Shared.Movement.Systems;
-using Content.Shared.Damage;
+using Content.Shared.Damage.Systems;
 using Content.Shared.DeadSpace.Necromorphs.CorpseCollector.Components;
 using Robust.Shared.Timing;
 using Content.Shared.Mobs.Components;
--- Content.Shared/DeadSpace/Ports/TapeRecorder/SharedTapeRecorderSystem.cs
+++ Content.Shared/DeadSpace/Ports/TapeRecorder/SharedTapeRecorderSystem.cs
@@ -1,5 +1,5 @@
 using Content.Shared.Containers.ItemSlots;
-using Content.Shared.Damage;
+using Content.Shared.Damage.Systems;
 using Content.Shared.Destructible;
 using Content.Shared.DoAfter;
 using Content.Shared.Examine;
